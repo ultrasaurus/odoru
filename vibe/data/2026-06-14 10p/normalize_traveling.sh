4922b90f4c30ea338a61225ecb714447c6866cb0
Sarah Allen <sarah@ultrasaurus.com>
2026-06-18 18:13:39 -0700

    prep to gitignore vibe/data

--- vibe/data/2026-06-14 10p/normalize_traveling.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")/.."
-: > data/augment_traveling_normalized.txt
-while IFS= read -r line; do
-  cargo run -q -- normalize "$line" >> data/augment_traveling_normalized.txt
-done < data/augment_traveling.txt
